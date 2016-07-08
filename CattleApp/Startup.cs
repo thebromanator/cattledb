cc5cfda0716b794d3bd0e3d8d9002d387d02f0e9
Mike <thebromanator@gmail.com>
2016-07-07 19:11:58 -0500

    Creating project

---
+++ CattleApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CattleApp.Startup))]
+namespace CattleApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
